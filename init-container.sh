e1af70af4156da327eeacc2d0edda3e32d80175d
dominikn <dominik.nwk@gmail.com>
2021-01-19 14:35:01 +0100

    added a log when connection with Husarnet is being established

--- init-container.sh
+++ init-container.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 sysctl net.ipv6.conf.lo.disable_ipv6=0
+
+echo "starting Husarnet daemon"
 husarnet daemon > /dev/null 2>&1 &
-sleep 5
+
+for i in {1..5}
+do
+    sleep 1
+    echo "checking status ($i): "
+    husarnet status
+done
+
 nginx
 husarnet join ${JOINCODE} ${HOSTNAME}
-sleep 5
+
+sleep 3
+
 python3 websocket_server.py &
 /bin/bash
