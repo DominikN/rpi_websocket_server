334c2dee9502e5ebb368c576c0a18b15a2bf1666
dominikn <dominik.nwk@gmail.com>
2021-01-19 15:55:36 +0100

    execute husarnet join after connection to a base server

--- init-container.sh
+++ init-container.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
+function parse_output() {
+    local str="ERROR"
+    local retval="success"
+
+    while read line; do
+        # echo "line: ${line}"
+        if [[ $line == *"$str"* ]]; then
+            retval="error"
+        fi
+    done
+    echo $retval
+}
+
 sysctl net.ipv6.conf.lo.disable_ipv6=0
 
 echo "starting Husarnet daemon"
 husarnet daemon > /dev/null 2>&1 &
 
-for i in {1..5}
+for i in {1..10}
 do
     sleep 1
-    echo "checking status ($i): "
-    husarnet status
+    
+    output=$( parse_output < <(husarnet status) )
+    echo "checking status ($i)($output): "
+    if [[ $output != "error" ]]; then
+        break
+    fi
 done
 
 nginx
 husarnet join ${JOINCODE} ${HOSTNAME}
 
-sleep 3
+sleep 2
 
 python3 websocket_server.py &
 /bin/bash
